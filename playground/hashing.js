f4e725858e4e201b149a78eb5ebe0c41de04d911
giabang777 <giabang777@gmail.com>
2017-10-26 14:42:42 +0700

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,10 +1,23 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data ={
-  id:10
-}
-var token = jwt.sign(data,'123abc');
-console.log(token);
+var password = '123abc!'
 
-var decoded = jwt.verify(token+"2",'123abc');
-console.log(decoded);
+bcrypt.genSalt(10,(err,salt) => {
+  bcrypt.hash(password,salt,(err,hash) => {
+    console.log(hash);
+  });
+});
+var hashpass = '$2a$10$KBhbIht.D2OUhn9mNGZqQuKF3lnS7tG6MGtjj4i4Hz9lhq5Rhqf/.';
+bcrypt.compare('123abc!',hashpass,(err,result) => {
+  console.log(result);
+})
+
+// var data ={
+//   id:10
+// }
+// var token = jwt.sign(data,'123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token+"2",'123abc');
+// console.log(decoded);
